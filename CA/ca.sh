981386940b519cd6e7574d2ce2f216f9b4739c1b
Joseph Kinyanjui <nesski@nesski.local>
2020-12-18 19:52:32 +0100

    updates

--- CA/ca.sh
+++ CA/ca.sh
@@ -4,10 +4,10 @@
 # Become a Certificate Authority
 ######################
 
-# # Generate private key
-# openssl genrsa -des3 -out myCA.key 2048
-# # Generate root certificate
-# openssl req -x509 -new -nodes -key myCA.key -sha256 -days 825 -out myCA.pem
+# Generate private key
+openssl genrsa -des3 -out myCA.key 2048
+# Generate root certificate
+openssl req -x509 -new -nodes -key myCA.key -sha256 -days 825 -out myCA.pem
 
 ######################
 # Create CA-signed certs
